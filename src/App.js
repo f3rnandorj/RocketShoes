88af4f456831de239baac0660e5824720b0a52f6
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-10-12 08:51:21 -0300

    Adding Header index and Header Styles
    Criação e estilização do componente global Header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
-
+import Header from './components/Header';
 import Ways from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Ways />
       <GlobalStyle />
     </BrowserRouter>
